f13836950f0a5ca1c1440a1bf83f196b7146d19d
Luiz Amaro <luizbarueri@gmail.com>
2023-10-30 14:17:00 -0300

    mudar tamanho da imagem para tela pequena

--- src/app/page.js
+++ src/app/page.js
@@ -33,8 +33,8 @@ export default function Home() {
           className={styles.logo}
           src="/esg.png"
           alt="ESG"
-          width={800}
-          height={500}
+          width={400}
+          height={300}
           priority
         />
       </div>
